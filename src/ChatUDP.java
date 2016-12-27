cbb19558e8fe0c8cbb97994b3260a364f33af74e
denny7794 <denny7794@gmail.com>
2016-12-27 11:35:53 +0400

    Initial commit

---
+++ src/ChatUDP.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Denis on 27.12.2016.
+ */
+public class ChatUDP {
+    private class thdReceiver extends Thread {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
